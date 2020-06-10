25541c8a733ebcea2570b3abd717aa5039b59a66
Apurva <58025903+apurvagurme@users.noreply.github.com>
2020-06-10 22:44:59 +0530

    Added bfs function

--- src/graph.js
+++ src/graph.js
@@ -1,11 +1,40 @@
-//Example 
+//Example
 // Pairs => [[from,to],[to,from]]
 // Source => from
-// To => to 
+// To => to
 // Should return true.
 
-const bfs = function(pairs,source,target){
-  
+const getAdjacencyTable = function (pairs) {
+	adjacencyTable = {};
+	pairs.forEach((pair) => {
+		let key = pair[0];
+		let value = pair[1];
+		if (!adjacencyTable[key]) adjacencyTable[key] = [];
+		adjacencyTable[key].push(value);
+	});
+	return adjacencyTable;
 };
 
-module.exports = {bfs};
+const bfs = function (pairs, source, target) {
+	const adjacencyTable = getAdjacencyTable(pairs);
+	const queue = [];
+	const visited = [];
+	queue.push(source);
+
+	while (queue.length > 0) {
+		const currentlyVisited = queue.shift();
+		if (currentlyVisited == target) return true;
+
+		if (adjacencyTable[currentlyVisited]) {
+			visited.push(currentlyVisited);
+			adjacencyTable[currentlyVisited].forEach((node) => {
+				if (!visited.includes(node) && !queue.includes(node)) {
+					queue.push(node);
+				}
+			});
+		}
+	}
+	return false;
+};
+
+module.exports = { bfs };
